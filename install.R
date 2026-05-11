bdd626cd300a21754b903387aa28fa15ee3afa86
R.C. Miessler <rmiessle@gettysburg.edu>
2026-05-11 16:25:53 -0400

    Update install.R

--- install.R
+++ install.R
@@ -2,14 +2,7 @@ install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
 
 install.packages("tidyverse")
-install.packages("ggplot2")
 install.packages("tidytext")
-install.packages("readr")
-install.packages("dplyr")
-install.packages("stringr")
 install.packages("lubridate")
 
 install.packages("syuzhet")
-
-install.packages("wordcloud")
-install.packages("RColorBrewer")
